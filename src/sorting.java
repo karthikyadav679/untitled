13bd61be11afed6c6a47656cdb72bfc2f6f60682
karthikyadav679 <46472910+karthikyadav679@users.noreply.github.com>
2021-11-02 18:03:53 +0530

    Update sorting.java

--- src/sorting.java
+++ src/sorting.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class sorting {
+import com.myproject.EmployeeRepository;
+
+
+public class sorting{
+    public static void main(String[] args) {
+
+        EmployeeRepository employeeRepository=new EmployeeRepository();
+
+        employeeRepository.getAll().forEach(System.out::println);
+        System.out.println("finding employee by Name: Karthik as first name");
+        System.out.println(employeeRepository.findEmployeeByFirstName("Karthik"));
+        System.out.println("Total Salary of the employees is:"+employeeRepository.findSalarySum());
+        System.out.println("Highest Salary of the employees is:"+employeeRepository.findEmployeeWithHighestSalary());
+        System.out.println("Sorting By Employeeid then Name:");
+        employeeRepository.sortByEmployeeId().forEach(System.out::println);
+        System.out.println();
+        System.out.println(employeeRepository.findSeniorMostEmployee());
+
+        System.out.println();
+        employeeRepository.sortByDepartment();
+        employeeRepository.NoofEmployeesInDepartment();
+
+        employeeRepository.sortByNameEmployee().forEach(System.out::println);
+
+        employeeRepository.NoofEmployeesHighestDepartment();
+
+        employeeRepository.EmployeeWithoutDepartment();
+        employeeRepository.SalaryIncrement();
+    }
+
+
 }
