c6a9dc1f89111401b82bfc29edc4a5f485e43705
karthikyadav679 <46472910+karthikyadav679@users.noreply.github.com>
2021-11-02 18:05:20 +0530

    Update Employee.java

--- src/com/myproject/Employee.java
+++ src/com/myproject/Employee.java
@@ -1,4 +1,131 @@
 package com.myproject;
 
+import java.time.LocalDate;
+import java.util.Objects;
+
 public class Employee {
+    private int employeeId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private LocalDate hiredate;
+    private String designation;
+    private double salary;
+    private int managerId;
+    private Department department;
+
+    public Employee() {
+    }
+
+    public Employee(int employeeId, String firstName, String lastName, String email, String phoneNumber, LocalDate hiredate, String designation, double salary, int managerId, Department department) {
+        this.employeeId = employeeId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.hiredate = hiredate;
+        this.designation = designation;
+        this.salary = salary;
+        this.managerId = managerId;
+        this.department = department;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public LocalDate getHiredate() {
+        return hiredate;
+    }
+
+    public void setHiredate(LocalDate hiredate) {
+        this.hiredate = hiredate;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public int getManagerId() {
+        return managerId;
+    }
+
+    public void setManagerId(int managerId) {
+        this.managerId = managerId;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "employeeId=" + employeeId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", hiredate=" + hiredate +
+                ", designation='" + designation + '\'' +
+                ", salary=" + salary +
+                ", managerId=" + managerId +
+                ", department=" + department +
+                '}';
+    }
+
+
 }
