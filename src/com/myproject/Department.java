60bf5299e30d92467d19a2483021dbada3499bb9
karthikyadav679 <46472910+karthikyadav679@users.noreply.github.com>
2021-11-02 18:04:35 +0530

    Update Department.java

--- src/com/myproject/Department.java
+++ src/com/myproject/Department.java
@@ -1,4 +1,53 @@
 package com.myproject;
 
-public class DeparTment {
+import java.util.Objects;
+
+public class Department {
+    private int departmentId;
+    private String departmentName;
+    private int managerId;
+
+    public Department() {
+    }
+
+    public int getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(int departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public int getManagerId() {
+        return managerId;
+    }
+
+    public void setManagerId(int managerId) {
+        this.managerId = managerId;
+    }
+
+    public Department(int departmentId, String departmentName, int managerId) {
+        this.departmentId = departmentId;
+        this.departmentName = departmentName;
+        this.managerId = managerId;
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "departmentId=" + departmentId +
+                ", departmentName='" + departmentName + '\'' +
+                ", managerId=" + managerId +
+                '}';
+    }
+
+
 }
